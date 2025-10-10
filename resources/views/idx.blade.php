93ca80fb7d9766769093bef556510b2b010a3300
reksa53 <rreksa53@gmail.com>
2025-10-11 00:22:32 +0700

    template view

---
+++ resources/views/idx.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>kontol e ahok cilek
+    </title>
+</head>
+<body>
+    <div>
+        [punya kayeye panjang]
+    </div>
+</body>
+</html>
