e58c291a080a45238234ddd489769af824fdad15
reksa53 <rreksa53@gmail.com>
2025-10-11 17:02:02 +0700

    login dan register

---
+++ resources/views/navbar/navigation.blade.php
@@ -0,0 +1,23 @@
+@yield
+<div class="sidebar" id="sidebar">
+        <div class="sidebar__logo">
+            <h4><i class="fas fa-leaf"></i> <span>BANK SAMPAH</span></h4>
+        </div>
+        <ul class="nav flex-column">
+            <li class="nav-item sidebar__nav-item">
+                <a class="nav-link sidebar__nav-link active" href="index.html"><i class="fas fa-home"></i> <span>Dashboard</span></a>
+            </li>
+            <li class="nav-item sidebar__nav-item">
+                <a class="nav-link sidebar__nav-link" href="riwayat-setoran.html"><i class="fas fa-recycle"></i> <span>Riwayat Setoran</span></a>
+            </li>
+            <li class="nav-item sidebar__nav-item">
+                <a class="nav-link sidebar__nav-link" href="penarikan-dana.html"><i class="fas fa-wallet"></i> <span>Penarikan Dana</span></a>
+            </li>
+            <li class="nav-item sidebar__nav-item">
+                <a class="nav-link sidebar__nav-link" href="pengaturan-akun.html"><i class="fas fa-cog"></i> <span>Pengaturan Akun</span></a>
+            </li>
+            <li class="nav-item mt-3 sidebar__nav-item">
+                <a class="nav-link sidebar__nav-link sidebar_logout" href="keluar.html"><i class="fas fa-sign-out-alt"></i> <span>Keluar</span></a>
+            </li>
+        </ul>
+    </div>
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,8 @@
 use Illuminate\Support\Facades\Route;
 
 
-Route::get('/', function () {
-    return view('idx');
-});
+// Route::get('/login', function () {
+//     return view('idx');
+// });
+
+Route::get('/', [App\Http\Controllers\Auth::class, 'index'])->name('login');
